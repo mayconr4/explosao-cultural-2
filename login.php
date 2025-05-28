387f5b49f3249ac9ee5279eea659084152557a44
mayconr4 <mayconhs2007@gmail.com>
2025-05-27 23:02:53 -0300

    adiociona login

--- login.php
+++ login.php
@@ -58,43 +58,67 @@ if (isset($_POST['entrar'])) {
 
 <!doctype html>
 <html lang="pt-br">
-
 <head>
-  <meta charset="utf-8" />
-  <meta name="viewport" content="width=device-width, initial-scale=1" />
-  <title>Explosão Cultural - Login</title>
-  <link rel="stylesheet" href="css/estilo.css" />
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" rel="stylesheet" />
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Explosão Cultural</title>
+  <link rel="shortcut icon" href="images/logotipo2.png" type="image/png" sizes="64x64"> 
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" rel="stylesheet">
+  <link rel="stylesheet" href="css/estilo.css">
 </head>
-
-<body class="bg-dark text-light">
-  <header class="bg-black p-3">
-
+<body class="bg-ligth text-dark">
+  <header class="bg-ligth p-3">
     <div class="container d-flex justify-content-between align-items-center">
-      <h1 class="m-0"><a href="index.php" class="text-light text-decoration-none">Explosão Cultural </a></h1>
-      <nav class="navbar navbar-expand-lg navbar-dark">
+      <h1 class="m-0"><a href="index.php" class="text-light text-decoration-none"><img class="logotipo" src="images/logo2.png" alt="logo tipo"></a></h1>
+      <nav class="navbar navbar-expand-lg navbar-light bg-white">
         <div class="container">
           <button class="navbar-toggler" type="button" id="menuBtn" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
           </button>
+
           <div class="collapse navbar-collapse" id="menuNav">
             <ul class="navbar-nav ms-auto">
-              <li class="nav-item"><a class="nav-link" href="index.php">Home</a></li>
-              <li class="nav-item"><a class="nav-link" href="generos.php">Gêneros</a></li>
-              <li class="nav-item"><a class="nav-link" href="cria-conta.php">Cadastro</a></li>
+              <li class="nav-item">
+                <a class="nav-link text-black" href="index.php">Home</a>
+              </li>
+
+              <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle text-black" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                  Gêneros
+                </a>
+                <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                  <?php foreach ($listaDeGeneros as $generos) { ?>
+                    <li>
+                      <a class="dropdown-item" href="generos.php?tipo=<?= $generos['id'] ?>">
+                        <?= $generos['tipo'] ?>
+                      </a>
+                    </li>
+                  <?php } ?>
+                </ul>
+              </li>
+
+              <li class="nav-item">
+                <a class="nav-link text-black" href="cria-conta.php">Cadastro</a>
+              </li>
+
+              <li class="nav-item">
+                <a class="nav-link text-black" href="login.php">Login</a>
+              </li>
             </ul>
 
-            <div class="position-relative ms-3">
+            <div class="position-relative">
               <form autocomplete="off" class="d-flex" action="resultados.php" method="POST" onsubmit="return false" id="form-busca">
-                <input id="campo-busca" name="busca" class="form-control me-2" type="search" placeholder="Pesquise aqui" aria-label="Pesquise aqui" />
+                <input id="campo-busca" name="busca" class="form-control me-2" type="search" placeholder="Pesquise aqui" aria-label="Pesquise aqui">
               </form>
-              <div id="resultados" class="mt-3 position-absolute container bg-white text-dark shadow-lg p-3 rounded"></div>
-            </div>
 
+              <!-- Div manipulada pelo busca.js -->
+              <div id="resultados" class="mt-3 position-absolute container bg-white shadow-lg p-3 rounded"></div>
+            </div>
           </div>
         </div>
       </nav>
     </div>
+    <hr>
   </header>
 
   <main class="container my-5 h-100">
@@ -128,7 +152,7 @@ if (isset($_POST['entrar'])) {
     </div>
   </main>
 
-  <footer class="bg-black text-center py-3">
+  <footer class="bg-ligth text-center py-3">
     <p class="m-0">Explosão Cultural — Empresa fictícia criada por Maycon e Lucas &copy;</p>
   </footer>
 
