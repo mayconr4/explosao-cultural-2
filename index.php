f6b84c94707a81fed5be78b4d30b83638ca1657e
mayconr4 <mayconhs2007@gmail.com>
2025-06-02 03:13:27 -0300

    adiciona rodape

--- index.php
+++ index.php
@@ -144,16 +144,48 @@ $listaDeGeneros = $generoServico->listarTodos();
     </article>
   </main>
 
-  <footer class="bg-light py-4">
-    <div class="container text-center">
-      <a href="index.php"><img class="logotipo" src="images/logo2.png" alt="Logo do site"></a>
-      <ul class="nav justify-content-center">
-        <li class="nav-item"><a class="nav-link text-black" href="index.php">Home</a></li>
-        <li class="nav-item"><a class="nav-link text-black" href="cria-conta.php">Cadastro</a></li>
-        <li class="nav-item"><a class="nav-link text-black" href="login.php">Login</a></li>
+  <hr>
+  <footer class="bg-ligth py-4">
+    <div class="container d-flex justify-content-center align-items-center flex-column">
+      <h1 class="m-0">
+        <a href="index.php" class="text-light text-decoration-none">
+          <img class="logotipo" src="images/logo2.png" alt="Logo do site">
+        </a>
+      </h1>
+
+      <ul class="nav">
+        <li class="nav-item">
+          <a class="nav-link text-black" href="index.php">Home</a>
+        </li>
+
+       <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle text-black" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                  Gêneros
+                </a>
+                <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                  <?php foreach ($listaDeGeneros as $generos) { ?>
+                    <li>
+                      <a class="dropdown-item" href="eventosPorGeneros.php?id=<?= htmlspecialchars($generos['id']) ?>">
+                        <?= htmlspecialchars($generos['tipo']) ?>
+                      </a>
+                    </li>
+                  <?php } ?>
+                </ul>
+              </li>
+
+        <li class="nav-item">
+          <a class="nav-link text-black" href="cria-conta.php">Cadastro</a>
+        </li>
+
+        <li class="nav-item">
+          <a class="nav-link text-black" href="login.php">Login</a>
+        </li>
       </ul>
-      <p class="m-0">Explosão Cultural — Empresa fictícia criada por Maycon e Lucas &copy;</p>
     </div>
+
+    <p class="m-0 text-center">
+      Explosão Cultural — Empresa fictícia criada por Maycon e Lucas &copy;
+    </p>
   </footer>
 
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js"></script>
